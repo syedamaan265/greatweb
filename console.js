95d7d5a662696823532703c3dd0e6b198ae47ded
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2022-11-22 19:02:32 +0530

    updated console  file

--- console.js
+++ console.js
@@ -1,2 +1,10 @@
 //hey this is a comment
 console.log("hello world")
+console.log("hello world")//hey this is a comment
+console.log("hello world")
+funtion main(){
+  return null;
+}
+main()
+
+console.log(main)
